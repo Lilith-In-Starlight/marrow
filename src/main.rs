fa9fb3ca74a72e4a8b06c3374635fd59adc1c533
Lilith-In-Starlight <katie-and@ampersandia.net>
2026-01-24 22:08:53 -0500

    allow directories as inputs for batch deck processing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ use shrek_deck::tts::write_to_tts_dir;
 use shrek_deck::tts::CardShape;
 use shrek_deck::tts::SaveState;
 use shrek_deck::GetCardInfo;
+use std::path::Component;
+use std::path::Path;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 use clap::{command, Parser};
 
@@ -62,58 +65,10 @@ impl GetCardInfo for BloodlessCard {
 fn main() {
     let cli = Args::parse();
 
-    match parse_file::<BloodlessCard>(&cli.input) {
-        Ok(mut cards) => {
-            if cli.flask {
-                for card in &mut cards {
-                    card.card.back =
-                        "https://file.garden/ZJSEzoaUL3bz8vYK/bloodlesscards/flaskvack.png"
-                            .to_string();
-                }
-            }
-            let save = match SaveState::new_with_deck(cards) {
-                Ok(x) => x,
-                Err(x) => return eprintln!("{x}"),
-            };
-
-            let contents = match serde_json::to_string_pretty(&save) {
-                Ok(x) => x,
-                Err(err) => return eprintln!("{err}"),
-            };
-
-            if cli.tabletop {
-                if let Some(path) = cli.output.parent() {
-                    let Some(mut saved_object) = tts::get_saved_objects_dir() else {
-                        panic!("Failed to find saved objects dir")
-                    };
-                    saved_object.push(path);
-                    std::fs::create_dir_all(saved_object)
-                        .expect("Failed to create folders needed for output path");
-                }
-
-                let result = if cli.flask {
-                    write_to_tts_dir(cli.output, contents, include_bytes!("blood.png"))
-                } else {
-                    write_to_tts_dir(cli.output, contents, include_bytes!("card.png"))
-                };
-
-                match result {
-                    Ok(()) => (),
-                    Err(error) => eprintln!("{error}"),
-                }
-            } else {
-                match std::fs::write(cli.output, contents) {
-                    Ok(()) => (),
-                    Err(error) => eprintln!("{error}"),
-                }
-            }
-        }
-        Err(errors) => {
-            for x in errors {
-                eprintln!("{x}");
-                eprintln!();
-            }
-        }
+    if cli.input.is_file() {
+        file_input(cli);
+    } else if cli.input.is_dir() {
+        dir_input(&cli);
     }
 }
 
@@ -122,4 +77,109 @@ fn get_filegarden_link(name: &str) -> String {
         "https://hemolymph.net/cardimgs/{}.png",
         name.replace(' ', "").replace('ä', "a")
     )
+}
+
+fn dir_input(cli: &Args) {
+    for entry in WalkDir::new(&cli.input).follow_links(true) {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(err) => {
+                eprintln!("Error: {err}");
+                continue;
+            }
+        };
+
+        let ext = entry.path().extension();
+
+        if ext.is_none_or(|x| x != "marrow" && x != "mflask") {
+            continue;
+        }
+
+        let flask = ext.is_some_and(|x| x == "mflask");
+
+        let mut components = entry.path().components();
+
+        // Remove root dir of this path
+        for i in cli.input.components() {
+            components.next();
+        }
+
+        let rest = components.as_path();
+
+        let mut output = cli.output.clone();
+
+        output.push(rest);
+
+        println!("{}", output.display());
+
+        file_input(Args {
+            input: entry.path().to_path_buf(),
+            output,
+            tabletop: cli.tabletop,
+            flask,
+        });
+    }
+}
+
+fn file_input(cli: Args) {
+    let mut cards = match parse_file::<BloodlessCard>(&cli.input) {
+        Ok(cards) => cards,
+        Err(errors) => {
+            for x in errors {
+                eprintln!("{x}");
+                eprintln!();
+            }
+            return;
+        }
+    };
+
+    if cli.flask {
+        for card in &mut cards {
+            card.card.back =
+                "https://file.garden/ZJSEzoaUL3bz8vYK/bloodlesscards/flaskvack.png".to_string();
+        }
+    }
+    let save = match SaveState::new_with_deck(cards) {
+        Ok(x) => x,
+        Err(x) => return eprintln!("{x}"),
+    };
+
+    let contents = match serde_json::to_string_pretty(&save) {
+        Ok(x) => x,
+        Err(err) => return eprintln!("{err}"),
+    };
+
+    if cli.tabletop {
+        create_all_needed_folders(&cli.output);
+        let result = if cli.flask {
+            write_to_tts_dir(cli.output, contents, include_bytes!("blood.png"))
+        } else {
+            write_to_tts_dir(cli.output, contents, include_bytes!("card.png"))
+        };
+
+        match result {
+            Ok(()) => (),
+            Err(error) => eprintln!("{error}"),
+        }
+    } else {
+        let output = cli.output.with_extension("json");
+        match std::fs::write(output, contents) {
+            Ok(()) => (),
+            Err(error) => eprintln!("{error}"),
+        }
+    }
+}
+
+fn create_all_needed_folders(path: &Path) {
+    let path = if path.is_dir() {
+        path
+    } else {
+        path.parent().unwrap()
+    };
+    let Some(mut saved_object) = tts::get_saved_objects_dir() else {
+        panic!("Failed to find saved objects dir")
+    };
+    saved_object.push(path);
+    std::fs::create_dir_all(&saved_object)
+        .expect("Failed to create folders needed for output path");
 }
